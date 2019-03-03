cf0c048d41079a41aa40586e76200e984eafdc83
Finley McIlwaine <finleymcilwaine@gmail.com>
2019-03-02 23:50:11 -0700

    input parsing is a go!

---
+++ public/javascripts/parseInput.js
@@ -0,0 +1,80 @@
+/**
+ * Parses input for logical errors and outputs
+ * appropriate error messages if there are errors.
+ * Otherwise, sends input data through POST request.
+ * 
+ * @author Finley McIlwaine
+ */
+
+ /**
+  * 
+  * @param {str} relationString correctly formatted input relation string
+  * @param {str} dependenciesString correctly formatted string of functional dependencies
+  */
+function parseInput(relationString, dependenciesString) {
+  // Initialize schema and dependencies objects
+  let schema = {
+    name: relationString[0] !== '(' ? relationString.match(/^[A-Z]+/)[0] : undefined,
+    attributes: []
+  };
+  let dependencies = {
+    given: [],
+    minimal: [],
+    complete: []
+  };
+
+  // Extract relation attributes from input string and put them in the schema object.
+  schema.attributes = relationString.slice(schema.name ? schema.name.length + 1 : 1 , relationString.length - 1).split(',');
+
+  // Check for duplicate attributes in relation schema.
+  if (hasDuplicates(schema.attributes)) {
+    relationInput.setCustomValidity('Schema may not contain duplicate attributes.');
+    updateRelationErrorMsg();
+    return;  // THROW EXCEPTION!?!?!?
+  }
+
+  // Extract dependencies from input string and put them in the dependencies object.
+  if (dependenciesString != "") {
+    let deps   = dependenciesString.split(";");
+    for(let i = 0; i < deps.length; i++) {
+      let newFD = {
+        leftAttributes: [],
+        rightAttributes: []
+      }
+      let atts = deps[i].split('->');
+      for(let j = 0; j < atts.length; j++) {
+        let splitAtts = atts[j].split(',');
+        for(let k = 0; k < splitAtts.length; k++) {
+          if (schema.attributes.indexOf(splitAtts[k]) < 0) {
+            // FD attribute is not a member of relation attributes,
+            // output error and return.
+            dependencyInput.setCustomValidity('Attribute \'' + splitAtts[k] + '\' is not a valid attribute.');
+            updateDependencyErrorMsg();
+            return; // THROW EXCEPTION!?!?!?
+          }
+          else if (j%2 === 0) {
+            newFD.leftAttributes.push(splitAtts[k]);
+          }
+          else {
+            newFD.rightAttributes.push(splitAtts[k]);
+          }
+        }
+      }
+      dependencies.given.push(newFD);
+    }
+  }
+}
+
+/**
+ * Returns true if array contains duplicate elements,
+ * false otherwise.
+ * @param array array of values or objects
+ */
+function hasDuplicates(array) {
+  let seen = [];
+  for(let i = 0; i < array.length; i++) {
+    if (seen.indexOf(array[i]) > -1) return true;
+    seen.push(array[i]);
+  }
+  return false;
+}
--- public/javascripts/validateInput.js
+++ public/javascripts/validateInput.js
@@ -2,7 +2,8 @@
  * Validates the relation and dependency input formats.
  * If the input is in an invalid format, the appropriate
  * error messages are displayed. If format is correct,
- * the parse event is dispatched.
+ * the input is parsed, checked for logical errors.
+ * If no logical errors, the post request is made.
  * 
  * @author Finley McIlwaine
  */
@@ -14,6 +15,9 @@ let relationErrorMsg   = document.getElementById('relation-error');
 let dependencyErrorMsg = document.getElementById('dependency-error');
 const submitBtn        = document.getElementById('submit');
 
+// Submit button click event calls input format validation
+submitBtn.addEventListener('click', validateInputFormat);
+
 /**
  * If input format is valid, dispatch parse event.
  * Otherwise, display appropriate error messages.
@@ -22,32 +26,38 @@ function validateInputFormat() {
   const relationRegex = /^[A-Z]*\(([A-Z]+,)*[A-Z]+\)$/g;
   const trimmedRelationString = relationInput.value.toUpperCase().replace(/ /g,'');
 
-  const dependencyRegex = /^([A-Z]+(,[A-Z]+)*->[A-Z]+(,[A-Z]+)*)*$/g;
+  const dependencyRegex = /(^([A-Z]+(,[A-Z]+)*->[A-Z]+(,[A-Z]+)*){1}(;[A-Z]+(,[A-Z]+)*->[A-Z]+(,[A-Z]+)*)*$)|^$/;
   const trimmedDependencyString = dependencyInput.value.toUpperCase().replace(/ /g,'');
   
+  let relationCheck = false, dependencyCheck = false;
+
   // Check the trimmed relation input string
   if (trimmedRelationString === "") {
     relationInput.setCustomValidity('Please enter a relation.');
   }
   else if (!relationRegex.test(trimmedRelationString)) {
-    relationInput.setCustomValidity('Invalid relation format!');
+    relationInput.setCustomValidity('Invalid relation format.');
   }
   else {
     relationInput.setCustomValidity('');
-    if (dependencyRegex.test(trimmedDependencyString)) {
-      dependencyInput.setCustomValidity('');
-      // TO-DO
-      // CALL PARSE-INPUT FUNCTION THEN POST!!!
-    }
+    relationCheck = true;
   }
 
   // Check the trimmed dependency input string
   if (!dependencyRegex.test(trimmedDependencyString)) {
-    dependencyInput.setCustomValidity('Invalid functional dependency format!');
+    dependencyInput.setCustomValidity('Invalid functional dependency format.');
+  }
+  else {
+    dependencyInput.setCustomValidity('');
+    dependencyCheck = true;
   }
 
   updateRelationErrorMsg();
   updateDependencyErrorMsg();
+
+  if (relationCheck && dependencyCheck) {
+    parseInput(trimmedRelationString,trimmedDependencyString);
+  }
 };
 
 // Update the error messages beneath the input boxes.
@@ -67,6 +77,3 @@ dependencyInput.addEventListener('input', ()=>{
   dependencyInput.setCustomValidity('');
   updateDependencyErrorMsg();
 });
-
-// Submit button click event calls input format validation
-submitBtn.addEventListener('click', validateInputFormat);
